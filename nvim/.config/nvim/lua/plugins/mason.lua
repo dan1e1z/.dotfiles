69321ecef1f782431c45695d20086a57d2a5f4dd
Daniel Lindsay-Shad <danielzlindsayshad@gmail.com>
2025-11-26 12:48:02 +1100

    added mason tool installer

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,7 +1,11 @@
 return {
 	"williamboman/mason.nvim",
+	dependencies = {
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
 	cmd = "Mason",
 	event = "BufReadPre",
+
 	opts = {
 		ui = {
 			icons = {
@@ -11,4 +15,19 @@ return {
 			},
 		},
 	},
+
+	config = function(_, opts)
+		require("mason").setup(opts)
+
+		local mason_tool_installer = require("mason-tool-installer")
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"isort",
+				"black",
+				"eslint_d",
+			},
+		})
+	end,
 }
